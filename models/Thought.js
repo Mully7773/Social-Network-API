571e4adae0de16d8cf3a797226543034e4cd0111
Nick M <mully7773@gmail.com>
2022-02-12 20:30:36 -0600

    Added thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,40 @@
-//thought code
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/helpers');
+
+
+const thoughtSchema = new Schema({
+    thoughtText: { 
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtDate => dateFormat(createdAtDate) //this might not work
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema],
+},
+{
+    toJSON: {
+        getters: true,
+        virtuals: true,
+    },
+        id: false,
+});
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
